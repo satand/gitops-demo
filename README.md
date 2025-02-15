80d2a8f5010e7d2fd939fea9559814df4284ca38
Andrea Satriani <asatrian@redhat.com>
2025-02-15 15:54:44 +0100

    Add argocd clusters for peripheral cluster in the cluster hub argocd

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # gitops-demo
 
-## install and start cloud-provider-kind loadbalancer
-in a terminal execute
+## Prepare podman machine if you are using it
+Open a ssh session with the podman machine
+```bash
+podman machine ssh podman-machine-default
+```
+execute
+```bash
+sudo sysctl net.ipv4.ip_unprivileged_port_start=80
+```
+to permit use of ports >= 80 as unprivileged ports.
+Then close the ssh session
+```bash
+exit
+```
+
+## Install and start cloud-provider-kind loadbalancer
+In a terminal execute
 ```bash
 go install sigs.k8s.io/cloud-provider-kind@latest
 sudo cloud-provider-kind
 ```
-## install the other things
+## Install the other things
 ```bash
 in another terminal execute
 install.sh
 ```
 
-## test the applications deployed in gitops
+## Test the applications deployed in gitops
 cluster kind-hub
 ```bash
 curl "http://localhost/bar" ; echo
--- install.sh
+++ install.sh
@@ -2,20 +2,21 @@
 
 set -euo pipefail
 
-kind create cluster --config kind-hub.yml
-kind create cluster --config kind-01.yml
-kind create cluster --config kind-02.yml
+## Create clusters
+kind create cluster --config kind/kind-hub.yml
+kind create cluster --config kind/kind-01.yml
+kind create cluster --config kind/kind-02.yml
 
 INGRESS_DOMAIN="nip.io"
 
-## add helm charts repos
+## Add helm charts repos
 echo
 # helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add gitea-charts https://dl.gitea.io/charts/
 helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
-## install ingress-nginx
+## Install ingress-nginx
 echo
 # helm --kube-context kind-hub upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
 #   --set controller.nodeSelector."kubernetes\.io/hostname"=kind-control-plane \
@@ -32,7 +33,7 @@ kubectl --context kind-hub wait --namespace ingress-nginx  --for=condition=ready
 kubectl --context kind-01 wait --namespace ingress-nginx  --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
 kubectl --context kind-02 wait --namespace ingress-nginx  --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
 
-## install Gitea
+## Install Gitea
 echo
 GITEA_HOST="gitea.${INGRESS_DOMAIN}"
 GITEA_USERNAME=gitea_admin
@@ -60,10 +61,9 @@ GITEA_EXTERNAL_IP=$(kubectl --context kind-hub -n gitea get svc/gitea -o=jsonpat
 GITEA_EXTERNAL_PORT=$(kubectl --context kind-hub -n gitea get svc/gitea -o=jsonpath='{.status.loadBalancer.ingress[0].ports[0].port}')
 GITEA_EXTERNAL_AUTHORITY="${GITEA_EXTERNAL_IP}:${GITEA_EXTERNAL_PORT}"
 
-## configure Gitea
+## Create the git repo
 echo
 sleep 15
-# Create repo
 curl -v -s -XPOST -H "Content-Type: application/json" -k -u "${GITEA_USERNAME}:${GITEA_PASSWORD}" \
   --url "http://${GITEA_HOST}/api/v1/user/repos" -d '{"name": "test-repo", "private": false, "default_branch": "main"}'
 # Push code to git repository
@@ -77,17 +77,17 @@ git remote add origin http://gitea.nip.io/gitea_admin/test-repo.git
 git push -u origin main
 cd ..
 
-## setup ArgoCD
+## Setup ArgoCDs
 echo
 ARGOCD_HOST="argocd.${INGRESS_DOMAIN}"
 cat <<EOF | helm --kube-context kind-hub upgrade --install argocd argo/argo-cd --wait --create-namespace --namespace=argocd --values=-
 configs:
   cm:
-    admin.enabled: false
+    admin.enabled: true
     timeout.reconciliation: 10s
   params:
     server.insecure: true
-    server.disable.auth: true
+    server.disable.auth: false
   repositories:
     local:
       name: local
@@ -102,11 +102,11 @@ ARGOCD01_HOST="argocd01.${INGRESS_DOMAIN}"
 cat <<EOF | helm --kube-context kind-01 upgrade --install argocd argo/argo-cd --wait --create-namespace --namespace=argocd --values=-
 configs:
   cm:
-    admin.enabled: false
+    admin.enabled: true
     timeout.reconciliation: 10s
   params:
     server.insecure: true
-    server.disable.auth: true
+    server.disable.auth: false
   repositories:
     local:
       name: local
@@ -121,11 +121,11 @@ ARGOCD02_HOST="argocd02.${INGRESS_DOMAIN}"
 cat <<EOF | helm --kube-context kind-02 upgrade --install argocd argo/argo-cd --wait --create-namespace --namespace=argocd --values=-
 configs:
   cm:
-    admin.enabled: false
+    admin.enabled: true
     timeout.reconciliation: 10s
   params:
     server.insecure: true
-    server.disable.auth: true
+    server.disable.auth: false
   repositories:
     local:
       name: local
@@ -135,8 +135,43 @@ server:
     enabled: true
     hostname: ${ARGOCD02_HOST}
 EOF
+ARGOCD_PASSWORD=$(kubectl --context kind-hub -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+ARGOCD01_PASSWORD=$(kubectl --context kind-01 -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+ARGOCD02_PASSWORD=$(kubectl --context kind-02 -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
-# Create an ArgoCD Application to monitor my local repository under mnt
+
+# Create peripheral argocd cluster in cluster hub argocd instance
+# 1) Setup kubeconfig in hub-control-plane (container of cluster hub control-plane node)
+# 2) Install argocd cli
+# 3) Use argocd cli to create c01 and c02 argocd clusters in the hub argocd
+echo
+docker cp ~/.kube/config hub-control-plane:/root/.kube/config
+
+export INTERNAL_IPADDRESS_HUB=$(kubectl --context kind-hub get nodes hub-control-plane -o jsonpath="{.status.addresses[?(@.type=='InternalIP')].address}" | awk '{print $1}')
+export INTERNAL_IPADDRESS_01=$(kubectl --context kind-01 get nodes 01-control-plane -o jsonpath="{.status.addresses[?(@.type=='InternalIP')].address}" | awk '{print $1}')
+export INTERNAL_IPADDRESS_02=$(kubectl --context kind-02 get nodes 02-control-plane -o jsonpath="{.status.addresses[?(@.type=='InternalIP')].address}" | awk '{print $1}')
+export ARGOCD_CLI_VERSION=$(curl -L -s https://raw.githubusercontent.com/argoproj/argo-cd/stable/VERSION)
+
+docker exec -i hub-control-plane /bin/bash <<EOF
+unset KUBECONFIG
+
+kubectl config set-cluster kind-hub --server https://$INTERNAL_IPADDRESS_HUB:6443
+kubectl config set-cluster kind-01 --server https://$INTERNAL_IPADDRESS_01:6443
+kubectl config set-cluster kind-02 --server https://$INTERNAL_IPADDRESS_02:6443
+
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/download/v$ARGOCD_CLI_VERSION/argocd-linux-amd64
+install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+kubectl config use-context kind-hub
+kubectl config set-context --current --namespace=argocd
+argocd login --core
+argocd cluster add kind-01 --yes --name c01
+argocd cluster add kind-02 --yes --name c02
+EOF
+
+
+# Create ArgoCD Applications
 echo
 cat <<EOF | kubectl --context kind-hub apply -f -
 apiVersion: argoproj.io/v1alpha1
@@ -170,22 +205,22 @@ spec:
         maxDuration: 10m # the maximum amount of time allowed for the backoff strategy
 EOF
 echo
-cat <<EOF | kubectl --context kind-01 apply -f -
+cat <<EOF | kubectl --context kind-hub apply -f -
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
-  name: test-app
+  name: test-app-c01
   namespace: argocd
   finalizers:
   - resources-finalizer.argocd.argoproj.io
 spec:
   destination:
     namespace: default
-    server: 'https://kubernetes.default.svc'
+    name: c01
   project: default
   source:
     path: apps
-    repoURL: "http://${GITEA_EXTERNAL_AUTHORITY}/gitea_admin/test-repo.git"
+    repoURL: http://gitea-http.gitea.svc.cluster.local:3000/gitea_admin/test-repo.git
     targetRevision: HEAD
   syncPolicy:
     automated:
@@ -202,22 +237,22 @@ spec:
         maxDuration: 10m # the maximum amount of time allowed for the backoff strategy
 EOF
 echo
-cat <<EOF | kubectl --context kind-02 apply -f -
+cat <<EOF | kubectl --context kind-hub apply -f -
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata:
-  name: test-app
+  name: test-app-c02
   namespace: argocd
   finalizers:
   - resources-finalizer.argocd.argoproj.io
 spec:
   destination:
     namespace: default
-    server: 'https://kubernetes.default.svc'
+    name: c02
   project: default
   source:
     path: apps
-    repoURL: "http://${GITEA_EXTERNAL_AUTHORITY}/gitea_admin/test-repo.git"
+    repoURL: http://gitea-http.gitea.svc.cluster.local:3000/gitea_admin/test-repo.git
     targetRevision: HEAD
   syncPolicy:
     automated:
@@ -233,11 +268,79 @@ spec:
         factor: 2 # a factor to multiply the base duration after each failed retry
         maxDuration: 10m # the maximum amount of time allowed for the backoff strategy
 EOF
+# echo
+# cat <<EOF | kubectl --context kind-01 apply -f -
+# apiVersion: argoproj.io/v1alpha1
+# kind: Application
+# metadata:
+#   name: test-app
+#   namespace: argocd
+#   finalizers:
+#   - resources-finalizer.argocd.argoproj.io
+# spec:
+#   destination:
+#     namespace: default
+#     server: 'https://kubernetes.default.svc'
+#   project: default
+#   source:
+#     path: apps
+#     repoURL: "http://${GITEA_EXTERNAL_AUTHORITY}/gitea_admin/test-repo.git"
+#     targetRevision: HEAD
+#   syncPolicy:
+#     automated:
+#       prune: true
+#       selfHeal: true
+#       allowEmpty: true
+#     syncOptions:
+#       - CreateNamespace=true
+#     retry:
+#       limit: -1 # number of failed sync attempt retries; unlimited number of attempts if less than 0
+#       backoff:
+#         duration: 5s # the amount to back off. Default unit is seconds, but could also be a duration (e.g. "2m", "1h")
+#         factor: 2 # a factor to multiply the base duration after each failed retry
+#         maxDuration: 10m # the maximum amount of time allowed for the backoff strategy
+# EOF
+# echo
+# cat <<EOF | kubectl --context kind-02 apply -f -
+# apiVersion: argoproj.io/v1alpha1
+# kind: Application
+# metadata:
+#   name: test-app
+#   namespace: argocd
+#   finalizers:
+#   - resources-finalizer.argocd.argoproj.io
+# spec:
+#   destination:
+#     namespace: default
+#     server: 'https://kubernetes.default.svc'
+#   project: default
+#   source:
+#     path: apps
+#     repoURL: "http://${GITEA_EXTERNAL_AUTHORITY}/gitea_admin/test-repo.git"
+#     targetRevision: HEAD
+#   syncPolicy:
+#     automated:
+#       prune: true
+#       selfHeal: true
+#       allowEmpty: true
+#     syncOptions:
+#       - CreateNamespace=true
+#     retry:
+#       limit: -1 # number of failed sync attempt retries; unlimited number of attempts if less than 0
+#       backoff:
+#         duration: 5s # the amount to back off. Default unit is seconds, but could also be a duration (e.g. "2m", "1h")
+#         factor: 2 # a factor to multiply the base duration after each failed retry
+#         maxDuration: 10m # the maximum amount of time allowed for the backoff strategy
+# EOF
 
 echo
-echo "Gitea address: http://${GITEA_HOST}"
-echo "Gitea login: U: ${GITEA_USERNAME} - P: ${GITEA_PASSWORD}"
+echo "Gitea" 
+echo "address: http://${GITEA_HOST} - login: U: ${GITEA_USERNAME} - P: ${GITEA_PASSWORD}"
 echo
-echo "ArgoCD HUB address: http://${ARGOCD_HOST}"
-echo "ArgoCD 01 address: http://${ARGOCD01_HOST}:8080"
-echo "ArgoCD 02 address: http://${ARGOCD02_HOST}:9080"
+echo "ArgoCD HUB"
+echo "address: http://${ARGOCD_HOST} - login: U: admin - P: ${ARGOCD_PASSWORD}"
+echo "ArgoCD 01"
+echo "address: http://${ARGOCD01_HOST}:8080 - login: U: admin - P: ${ARGOCD01_PASSWORD}"
+echo "ArgoCD 02"
+echo "address: http://${ARGOCD02_HOST}:9080 - login: U: admin - P: ${ARGOCD02_PASSWORD}"
+
